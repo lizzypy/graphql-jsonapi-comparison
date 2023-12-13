2f42c1cf0c2fe052b743ea4b9fa745f668e7291a
liamyafuso <liam@siliconhemisphere.com>
2023-12-12 20:13:02 -0800

    reorganized dockerfile/fixed frontend build/remove log files/add frontend container

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,15 +10,13 @@ const client = new ApolloClient({
     cache: new InMemoryCache(),
 });
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
       <ApolloProvider client={client}>
           <App />
       </ApolloProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root') as HTMLElement
 );
 
 // If you want to start measuring performance in your app, pass a function
